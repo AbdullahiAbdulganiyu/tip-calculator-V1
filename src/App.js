5ede347767a92a9975b7b8aa527d2f295ed33ea3
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-24 15:48:07 +0100

    making the bill value empty by default

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   );
 }
 function TipCalculator() {
-  const [bill, setBill] = useState(0);
+  const [bill, setBill] = useState("");
   const [percentage1, setPercentage1] = useState(0);
   const [percentage2, setPercentage2] = useState(0);
   const tip = bill * ((percentage1 + percentage2) / 2 / 100);
