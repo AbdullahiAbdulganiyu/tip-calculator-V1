7f6eb140fbfdbbba8c25958bc1b6879ffe7d21be
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-24 14:01:24 +0100

    creating a function to handle the change

--- src/App.js
+++ src/App.js
@@ -11,12 +11,20 @@ export default function App() {
 
 function Bill() {
   const [amount, setAmount] = useState(0);
+  function handleChange(e) {
+    setAmount(e.target.value);
+  }
   return (
     <div>
       <p>
         How much was the Bill{" "}
         <span>
-          <input type="text" placeholder="enter bill" value={0} />
+          <input
+            type="text"
+            placeholder="enter bill"
+            value={amount}
+            onChange={handleChange}
+          />
         </span>
       </p>
     </div>
